7714ee35bc6342e7986daaf204ec9defbc78958e
Shivam <54470073+TeamATAuomationTeknix@users.noreply.github.com>
2021-03-18 15:11:09 +0530

    Add some more code

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ while count <= 10:
     print(count)
     count+=2
     
+name = input("Type your name :- ")
+surname = input("Type your surname :- ")
+if name == "AT":
+    print("cool")
+else:
+    print("Not so cool")
